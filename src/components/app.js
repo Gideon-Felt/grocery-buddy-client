f0a81d002569eb9f8031c4826bf2c79044b5e6b3
Gideon-Felt <gideon@feltslanding.com>
2020-02-07 16:27:09 -0700

    finnished store and product forms

--- src/components/app.js
+++ src/components/app.js
@@ -7,9 +7,22 @@ Icons()
 
 
 function App() {
-  const [ formSideBarStyles, setFormSideBarStyles] = useState({width: "0", marginLeft: "0"})
-  const [SearchSpaceStyle, setSearchSpaceStyle] = useState({width: "100vw", marginLeft: "0"})
-  const [loggedInStatus, setLoggedInStatus] = useState("NOT_LOGGED_IN")
+  const [ secondNavSideBarStyles, setSecondNavSideBarStyles ] = useState({width: "0", marginLeft: "0"})
+  const [ formSideBarStyles, setFormSideBarStyles ] = useState({width: "0", marginLeft: "0"})
+  const [ SearchSpaceStyle, setSearchSpaceStyle ] = useState({width: "100vw", marginLeft: "0"})
+  const [ loggedInStatus, setLoggedInStatus ] = useState("NOT_LOGGED_IN")
+
+  const [ storeName, setStoreName ] = useState("")
+  const [ address, setAddress ] = useState("")
+  const [ city, setCity ] = useState("")
+  const [ state, setState ] = useState("")
+  const [ zipCode, setZipCode ] = useState("")
+  
+  const [ storeId, setStoreId ] = useState("")
+  const [ productName, setProductName ] = useState("")
+  const [ price, setPrice ] = useState("")
+
+
   
 
   function openNav() {
@@ -18,9 +31,63 @@ function App() {
   }
 
   function closeNav() {
-    setFormSideBarStyles({width: "0", margimarginRightnLeft: "0"})
+    setFormSideBarStyles({width: "0", marginRigh: "0"})
     setSearchSpaceStyle({width: "100vw", marginLeft: "0"})
   }
+
+  function openNavSecond() {
+    setSecondNavSideBarStyles({width: "100vw", marginRight: "100vw"})
+    setFormSideBarStyles({width: "0", marginRight: "0"})
+    setSearchSpaceStyle({width: "0", marginLeft: "0"})
+  }
+
+  function closeNavSecond() {
+    setSecondNavSideBarStyles({width: "0", marginRight: "100Vw"})
+    setFormSideBarStyles({width: "100vw", marginLeft: "0"})
+    setSearchSpaceStyle({width: "0", marginLeft: "-100vw"})
+  }
+
+
+
+  function handleStoreSubmit(event) {
+    axios
+    .post("localhost:5000/add-store", {
+      "store_name" : storeName,
+      "address" : address,
+      "city" : city,
+      "state" : state,
+      "zip_code" : zipCode
+    })
+    .then(response => {
+      setStoreName('')
+      setAddress('')
+      setCity('')
+      setState('')
+      setZipCode('')
+      })
+    .catch(err => {
+      console.log("POST new store error:", err)
+    })
+  }
+
+  function handleProductSubmit(event) {
+    axios
+    .post("localhost:5000/add-product", {
+      "store_id" : storeId,
+      "product_name" : productName,
+      "price" : price
+    })
+    .then(response => {
+      setStoreId('')
+      setProductName('')
+      setPrice('')
+      })
+    .catch(err => {
+      console.log("POST new store error:", err)
+    })
+  }
+
+
 
   // useEffect(() => {
   //   checkLoginStatus()
@@ -46,20 +113,114 @@ function App() {
 
       <div className="app-body">
 
-        <div id="mySidebar" className="sidebar form" style={formSideBarStyles}>
+        <div className="sidebar form" style={secondNavSideBarStyles}>
+          <a href="#" className="closebtn" onClick={() => closeNavSecond()}>&times;</a>
+          <h3>Add a Product</h3>
+
+          <form onSubmit={handleProductSubmit} className="product-form">
+            <div className="store-form-wrapper">
+                <input
+                  className="form-input"
+                  type="text"
+                  onChange={(e) => setStoreId(e.target.value)}
+                  name="setStoreId"
+                  placeholder="store id:"
+                  value={storeId}
+                />
+
+                <input
+                  className="form-input"
+                  type="text"
+                  onChange={(e) => setProductName(e.target.value)}
+                  name="productName"
+                  placeholder="product name:"
+                  value={productName}
+                />
+
+                <input
+                  className="form-input"
+                  type="text"
+                  onChange={(e) => setPrice(e.target.value)}
+                  name="price"
+                  placeholder="price $"
+                  value={price}
+                />
+              </div>
+          </form>
+        </div>
+        
+
+        <div id="mySidebar" className="sidebar form" style={formSideBarStyles}> 
+          <a href="#" className="openSecond" onClick={() => openNavSecond()}>add Product</a>          
           <a href="#" className="closebtn" onClick={() => closeNav()}>&times;</a>          
-          <a href="#">Services</a>
-          <input className="form-input" type="text" placeholder="FORM INPUT"/>
+          <h3>Add a Store</h3>
+
+          
+          {/* FORM FOR SUBMITTING NEW PRODUCTS */}
+          <form onSubmit={handleStoreSubmit} className="store-form">
+          <div className="store-form-wrapper">
+            <input
+              className="form-input"
+              type="text"
+              onChange={(e) => setStoreName(e.target.value)}
+              name="storeName"
+              placeholder="store name:"
+              value={storeName}
+            />
+
+            <input
+              className="form-input"
+              type="text"
+              onChange={(e) => setAddress(e.target.value)}
+              name="address"
+              placeholder="address:"
+              value={address}
+            />
+
+            <input
+              className="form-input"
+              type="text"
+              onChange={(e) => setCity(e.target.value)}
+              name="city"
+              placeholder="city:"
+              value={city}
+            />
+
+            <input
+              className="form-input"
+              type="text"
+              onChange={(e) => setState(e.target.value)}
+              name="state"
+              placeholder="State:"
+              value={state}
+            />
+
+
+            <input
+              className="form-input"
+              type="text"
+              onChange={(e) => setZipCode(e.target.value)}
+              name="zipCode"
+              placeholder="zip code:"
+              value={zipCode}
+            />
+
+
+          </div>
+          <button className="nav-link-btn" onClick={(e) => { e.preventDefault}}>Save</button>
+        </form>
+
         </div>
 
-        <div className="search" style={SearchSpaceStyle}>
-          HELLO FROM SEARCH PAGE
-          <input className="serachBar" type="text" placeholder="Search"/>
-        </div>
+          <div className="search" style={SearchSpaceStyle}>
+            HELLO FROM SEARCH PAGE
+            <input className="serachBar" type="text" placeholder="Search"/>
+          </div>
 
-      </div>
-  </div>
+        </div>
+    </div>
   )
 }
+
 
 export default App
